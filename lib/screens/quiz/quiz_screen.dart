bc9b399374acb3d810c34235e85269dddcf12198
fitse-yotor <fitseyotor@gmail.com>
2021-06-24 10:57:00 +0300

    app bar for quiz screen

--- lib/screens/quiz/quiz_screen.dart
+++ lib/screens/quiz/quiz_screen.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class QuizScreen extends StatelessWidget {
   const QuizScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        actions: [
+          FlatButton(onPressed: () {}, child: Text("Skip")),
+        ],
+      ),
+      body: Stack(
+        children: [
+          SvgPicture.asset("assets/icons/bg.svg", fit: BoxFit.fill),
+        ],
+      ),
+    );
   }
 }
