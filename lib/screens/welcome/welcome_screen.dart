39b34fa916631ab680b04ea986ec9f2f32077020
fitse-yotor <fitseyotor@gmail.com>
2021-06-24 10:48:37 +0300

    quiz screen

--- lib/screens/welcome/welcome_screen.dart
+++ lib/screens/welcome/welcome_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/constants.dart';
 import 'package:websafe_svg/websafe_svg.dart';
 
 import 'package:flutter_svg/svg.dart';
@@ -16,7 +17,9 @@ class WelcomeScreen extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Spacer(),
+                Spacer(
+                  flex: 2,
+                ),
                 Text(
                   "Let's Play Quiz",
                   style: Theme.of(context).textTheme.headline4.copyWith(
@@ -33,8 +36,28 @@ class WelcomeScreen extends StatelessWidget {
                           borderRadius: BorderRadius.all(Radius.circular(12)))),
                 ),
                 Spacer(),
-                Container(),
-                Spacer()
+                InkWell(
+                  //onTap: () => Get.to(QuizScreen()),
+                  child: Container(
+                    width: double.infinity,
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.all(kDefaultPadding * 0.75), // 15
+                    decoration: BoxDecoration(
+                      gradient: kPrimaryGradient,
+                      borderRadius: BorderRadius.all(Radius.circular(12)),
+                    ),
+                    child: Text(
+                      "Lets Start Quiz",
+                      style: Theme.of(context)
+                          .textTheme
+                          .button
+                          .copyWith(color: Colors.black),
+                    ),
+                  ),
+                ),
+                Spacer(
+                  flex: 2,
+                )
               ],
             ),
           ))
