a77547e5da9bc68794370b0bc04f424718db69b9
fitse-yotor <fitseyotor@gmail.com>
2021-06-24 14:27:15 +0300

    finale

--- lib/screens/quiz/quiz_screen.dart
+++ lib/screens/quiz/quiz_screen.dart
@@ -1,123 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
-import 'package:quiz_app/constants.dart';
-import 'package:quiz_app/controller/question_controllr.dart';
+import 'package:quiz_app/controllers/question_controller.dart';
+
+import 'components/body.dart';
 
 class QuizScreen extends StatelessWidget {
-  const QuizScreen({Key key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
+    QuestionController _controller = Get.put(QuestionController());
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: AppBar(
+        // Fluttter show the back button automatically
         backgroundColor: Colors.transparent,
         elevation: 0,
         actions: [
-          FlatButton(onPressed: () {}, child: Text("Skip")),
+          FlatButton(onPressed: _controller.nextQuestion, child: Text("Skip")),
         ],
       ),
-      body: body(),
-    );
-  }
-}
-
-class body extends StatelessWidget {
-  const body({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        SvgPicture.asset("assets/icons/bg.svg", fit: BoxFit.fill),
-        SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              children: [
-                _progress(),
-                SizedBox(height: kDefaultPadding),
-                Text.rich(
-                  TextSpan(
-                    text: "Question 1",
-                    style: Theme.of(context)
-                        .textTheme
-                        .headline4
-                        .copyWith(color: kSecondaryColor),
-                    children: [
-                      TextSpan(
-                        text: "/10",
-                        style: Theme.of(context)
-                            .textTheme
-                            .headline5
-                            .copyWith(color: kSecondaryColor),
-                      ),
-                    ],
-                  ),
-                ),
-                Divider(
-                  thickness: 1.5,
-                )
-              ],
-            ),
-          ),
-        )
-      ],
-    );
-  }
-}
-
-class _progress extends StatelessWidget {
-  const _progress({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 30,
-      decoration: BoxDecoration(
-        border: Border.all(color: Color(0xFF3F4768), width: 3),
-        borderRadius: BorderRadius.circular(50),
-      ),
-      child: GetBuilder<QuestionController>(
-          init: QuestionController(),
-          builder: (controller) {
-            // print(controller.animation.value);
-
-            return Stack(
-              children: [
-                LayoutBuilder(
-                    builder: (context, Constraints) => Container(
-                          width:
-                              Constraints.maxWidth * controller.animation.value,
-                          decoration: BoxDecoration(
-                              gradient: kPrimaryGradient,
-                              borderRadius: BorderRadius.circular(50)),
-                        )),
-                Positioned.fill(
-                    child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: kDefaultPadding / 2),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                          "${((controller.animation.value * 60).round())} Sec"),
-                      SvgPicture.asset(
-                        "assets/icons/clock.svg",
-                      ),
-                    ],
-                  ),
-                ))
-              ],
-            );
-          }),
+      body: Body(),
     );
   }
 }
--- lib/screens/welcome/welcome_screen.dart
+++ lib/screens/welcome/welcome_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/constants.dart';
-import 'package:websafe_svg/websafe_svg.dart';
-
 import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
+import 'package:quiz_app/constants.dart';
+import 'package:quiz_app/screens/quiz/quiz_screen.dart';
 
 class WelcomeScreen extends StatelessWidget {
   @override
@@ -12,55 +12,54 @@ class WelcomeScreen extends StatelessWidget {
         children: [
           SvgPicture.asset("assets/icons/bg.svg", fit: BoxFit.fill),
           SafeArea(
-              child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Spacer(
-                  flex: 2,
-                ),
-                Text(
-                  "Let's Play Quiz",
-                  style: Theme.of(context).textTheme.headline4.copyWith(
-                      color: Colors.white, fontWeight: FontWeight.bold),
-                ),
-                Text("Enter your information Below"),
-                Spacer(),
-                TextField(
-                  decoration: InputDecoration(
-                      fillColor: Color(0xFF1C2341),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Spacer(flex: 2), //2/6
+                  Text(
+                    "Let's Play Quiz,",
+                    style: Theme.of(context).textTheme.headline4.copyWith(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                  Text("Enter your informations below"),
+                  Spacer(), // 1/6
+                  TextField(
+                    decoration: InputDecoration(
                       filled: true,
+                      fillColor: Color(0xFF1C2341),
                       hintText: "Full Name",
                       border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(12)))),
-                ),
-                Spacer(),
-                InkWell(
-                  //onTap: () => Get.to(QuizScreen()),
-                  child: Container(
-                    width: double.infinity,
-                    alignment: Alignment.center,
-                    padding: EdgeInsets.all(kDefaultPadding * 0.75), // 15
-                    decoration: BoxDecoration(
-                      gradient: kPrimaryGradient,
-                      borderRadius: BorderRadius.all(Radius.circular(12)),
-                    ),
-                    child: Text(
-                      "Lets Start Quiz",
-                      style: Theme.of(context)
-                          .textTheme
-                          .button
-                          .copyWith(color: Colors.black),
+                        borderRadius: BorderRadius.all(Radius.circular(12)),
+                      ),
                     ),
                   ),
-                ),
-                Spacer(
-                  flex: 2,
-                )
-              ],
+                  Spacer(), // 1/6
+                  InkWell(
+                    onTap: () => Get.to(QuizScreen()),
+                    child: Container(
+                      width: double.infinity,
+                      alignment: Alignment.center,
+                      padding: EdgeInsets.all(kDefaultPadding * 0.75), // 15
+                      decoration: BoxDecoration(
+                        gradient: kPrimaryGradient,
+                        borderRadius: BorderRadius.all(Radius.circular(12)),
+                      ),
+                      child: Text(
+                        "Lets Start Quiz",
+                        style: Theme.of(context)
+                            .textTheme
+                            .button
+                            .copyWith(color: Colors.black),
+                      ),
+                    ),
+                  ),
+                  Spacer(flex: 2), // it will take 2/6 spaces
+                ],
+              ),
             ),
-          ))
+          ),
         ],
       ),
     );
